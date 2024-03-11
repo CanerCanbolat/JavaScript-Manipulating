beedddf88a8d3e457bfc6f5421ae128d2cac61df
Caner Canbolat <107541646+CanerCanbolat@users.noreply.github.com>
2024-03-11 13:59:27 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const allLinks = document.querySelectorAll("a");
+
+// for (let link of allLinks) {
+//   link.innerText = "I AM A LINK!!!!";
+// }
+
+for (let link of allLinks) {
+  link.style.color = "rgb(0, 108, 134)";
+  link.style.textDecorationColor = "magenta";
+  link.style.textDecorationStyle = "wavy";
+}
+
+// const h1 = document.querySelector("h1");
+// h1.style.color = "green";
+// h1.style.fontSize = `3em`;
+// h1.style.border = `2px solid pink`;
